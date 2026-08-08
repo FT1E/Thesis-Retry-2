e0eac96ad9b16e83ebf59af6507edaa2055dee18
FT1E <ft1hcfu@gmail.com>
2026-08-09 00:25:52 +0200

    script for printing pairs - handwriting the matchings

---
+++ print_pairs.py
@@ -0,0 +1,45 @@
+
+
+raw = """GRAPH_ID VEHICLE_ID
+3 6
+11 2
+82 1
+7 5
+58 3
+32 2
+50 1
+2 6
+21 0
+67 0
+17 0
+54 0
+81 0
+6 11
+40 0
+71 0
+57 3
+36 0
+13 2
+30 0
+9 6
+66 0
+1 4
+10 6
+70 0
+16 0
+79 1
+73 1
+53 0
+49 3
+63 1
+5 11
+35 0"""
+
+pairs = []
+for line in raw.split('\n')[1:]:
+    numbers = line.split(' ')
+    numbers = [int(n) for n in numbers]
+    pairs.append(numbers)
+
+for pair in pairs:
+    print(f"{pair[0]} {pair[1]}")
